f0c5f370223ff3c795619c56a0a4777002a2a791
alphaion <alphaion89@gmail.com>
2022-08-13 00:08:54 +0530

    aos added

--- script/script.js
+++ script/script.js
@@ -5,3 +5,12 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
+AOS.init();
+
+const items = ["./res/images/heroImg01.svg", "./res/images/heroImg02.svg", "./res/images/heroImg03.svg", "./res/images/heroImg04.svg", "./res/images/heroImg05.svg"];
+
+function random_item(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+document.getElementById('mainImg').srcset = random_item(items);
